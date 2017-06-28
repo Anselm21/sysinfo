462ae62f90a9c81e8437e2e3d956017562494823
Alexander Turivnoi <a.turivnoi@yandex.ru>
2017-06-28 11:51:59 +0300

    refactored gathering sys info

--- main.py
+++ main.py
@@ -1,39 +1,31 @@
-from flask import Flask, request
-import psutil
-from tensorflow.python.client import device_lib
+from flask import Flask, request, jsonify
+# from tensorflow.python.client import device_lib
+from thread_sys import ThreadSys
 
 app = Flask(__name__)
+sys_info = ThreadSys()
 
 
 @app.route('/')
 def entry_page():
-    cpu_used = psutil.cpu_percent()
-    memory = psutil.virtual_memory()
-    memory_total = bytes_to_gib(memory.total)
-    memory_used = bytes_to_gib(memory.used)
-    gpus = get_available_gpus()
+    cpu_used = sys_info.cpu_used
+    memory_total = sys_info.memory_total
+    memory_used = sys_info.memory_used
+    rx_speed = sys_info.rx_speed
+    tx_speed = sys_info.tx_speed
+
     return """
     CPU LOAD: {}% 
     TOTAL MEMORY: {} GiB  
     MEMORY USED: {} GiB 
-    GPUs NUMBER: {} """.format(cpu_used, memory_total, memory_used, gpus)
+    RX_SPEED: {} Mbps
+    TX_SPEED: {} Mbps""".format(cpu_used, memory_total, memory_used, rx_speed, tx_speed)
 
-
-def bytes_to_mb(bts):
-    mb = bts/1048576
-    return "{0:0.1f}".format(mb)
-
-
-def bytes_to_gib(bts):
-    gib = bts/1073741824
-    return "{0:0.1f}".format(gib)
-
-
-def get_available_gpus():
-    local_device_protos = device_lib.list_local_devices()
-    gpus = [x.name for x in local_device_protos if x.device_type == 'GPU']
-    return len(gpus)
-
+#
+# def get_available_gpus():
+#     local_device_protos = device_lib.list_local_devices()
+#     gpus = [x.name for x in local_device_protos if x.device_type == 'GPU']
+#     return len(gpus)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ thread_sys.py
@@ -0,0 +1,60 @@
+import psutil
+import time
+import threading
+
+
+class ThreadSys(object):
+
+    def __init__(self, interval=1):
+        """ Constructor
+        :type interval: int
+        :param interval: Check sys params interval, in seconds
+        """
+        self.interval = interval
+        self.tx_prev = 0
+        self.rx_prev = 0
+        self.tx_speed = 0
+        self.rx_speed = 0
+        self.cpu_used = 0
+        self.memory_total = 0
+        self.memory_used = 0
+
+        thread = threading.Thread(target=self.run, args=())
+        thread.daemon = True
+        thread.start()
+
+    @staticmethod
+    def bytes_to_mb(bts):
+        mb = bts / 1048576
+        return "{0:0.1f}".format(mb)
+
+    @staticmethod
+    def bytes_to_gib(bts):
+        gib = bts / 1073741824
+        return "{0:0.1f}".format(gib)
+
+    @staticmethod
+    def get_bytes():
+        network = psutil.net_io_counters()
+        return {'bytes_sent': network.bytes_sent, 'bytes_recv': network.bytes_recv}
+
+    def run(self):
+        """ Get sys info with 1s period """
+        while True:
+            self.cpu_used = psutil.cpu_percent()
+            memory = psutil.virtual_memory()
+            self.memory_total = self.bytes_to_gib(memory.total)
+            self.memory_used = self.bytes_to_gib(memory.used)
+            tx = self.get_bytes()['bytes_sent']
+            rx = self.get_bytes()['bytes_recv']
+
+            if self.tx_prev > 0:
+                self.tx_speed = self.bytes_to_mb(tx - self.tx_prev)
+
+            if self.rx_prev > 0:
+                self.rx_speed = self.bytes_to_mb(rx - self.rx_prev)
+
+            time.sleep(self.interval)
+
+            self.tx_prev = tx
+            self.rx_prev = rx
