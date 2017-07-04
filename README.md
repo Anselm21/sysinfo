b54e5e242c0d5c44d1c7cabb54b248ca3154890e
Alexander Turivnoi <a.turivnoi@yandex.ru>
2017-07-04 14:55:29 +0300

    readme instructions

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Script to gather system CPU and Network stats
+
+Install&Run:
+pip install -r requirements.txt
+python3 main.py
--- config.example.py
+++ config.example.py
@@ -1,2 +1,2 @@
-# Correct server name should be provided in config.py
+# Desired server name should be provided in config.py
 SERVER_NAME = 'SERVER_01'
