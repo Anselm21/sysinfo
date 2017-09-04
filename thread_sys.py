4432f137ff12bf585441c952fc34e0c785cad029
Alexander Turivnoi <a.turivnoi@yandex.ru>
2017-09-04 13:51:39 +0300

    restored threading to proper calc netStat

--- thread_sys.py
+++ thread_sys.py
@@ -1,13 +1,17 @@
 import psutil
 import time
+import threading
 import config
 
 
 class ThreadSys(object):
 
-    def __init__(self):
+    def __init__(self, interval=1):
         """ Constructor
+        :type interval: int
+        :param interval: Check sys params interval, in seconds
         """
+        self.interval = interval
         self.tx_prev = 0
         self.rx_prev = 0
         self.tx_speed = 0
@@ -15,7 +19,10 @@ class ThreadSys(object):
         self.cpu_used = 0
         self.memory_total = 0
         self.memory_used = 0
-        self.run()
+
+        thread = threading.Thread(target=self.run, args=())
+        thread.daemon = True
+        thread.start()
 
     @staticmethod
     def bytes_to_mb(bts):
@@ -33,18 +40,24 @@ class ThreadSys(object):
         return {'bytes_sent': network.bytes_sent, 'bytes_recv': network.bytes_recv}
 
     def run(self):
-        self.cpu_used = psutil.cpu_percent()
-        memory = psutil.virtual_memory()
-        self.memory_total = self.bytes_to_gib(memory.total)
-        self.memory_used = self.bytes_to_gib(memory.used)
-        tx = self.get_bytes()['bytes_sent']
-        rx = self.get_bytes()['bytes_recv']
-        if self.tx_prev > 0:
-            self.tx_speed = self.bytes_to_mb(tx - self.tx_prev)
-        if self.rx_prev > 0:
-            self.rx_speed = self.bytes_to_mb(rx - self.rx_prev)
-        self.tx_prev = tx
-        self.rx_prev = rx
+        """ Get sys info with 1s period """
+        while True:
+            self.cpu_used = psutil.cpu_percent()
+            memory = psutil.virtual_memory()
+            self.memory_total = self.bytes_to_gib(memory.total)
+            self.memory_used = self.bytes_to_gib(memory.used)
+            tx = self.get_bytes()['bytes_sent']
+            rx = self.get_bytes()['bytes_recv']
+
+            if self.tx_prev > 0:
+                self.tx_speed = self.bytes_to_mb(tx - self.tx_prev)
+
+            if self.rx_prev > 0:
+                self.rx_speed = self.bytes_to_mb(rx - self.rx_prev)
+            time.sleep(self.interval)
+
+            self.tx_prev = tx
+            self.rx_prev = rx
 
     def get_info(self):
         return {
