4d7a2f6bee35c795e7cb9c58aa501bc3191ed2df
Alexander Turivnoi <a.turivnoi@yandex.ru>
2017-09-08 11:17:32 +0300

    get cpu_usage and mem_stat without psutil

--- thread_sys.py
+++ thread_sys.py
@@ -3,6 +3,7 @@ import time
 import threading
 import config
 import os
+import decimal,timeit
 
 
 class ThreadSys(object):
@@ -20,6 +21,8 @@ class ThreadSys(object):
         self.cpu_used = 0
         self.memory_total = 0
         self.memory_used = 0
+        self.last_worktime = 0
+        self.last_idletime = 0
 
         thread = threading.Thread(target=self.run, args=())
         thread.daemon = True
@@ -36,56 +39,41 @@ class ThreadSys(object):
         return float("{0:0.1f}".format(gib))
 
     @staticmethod
+    def mb_to_gib(mb):
+        gib = mb / 1024
+        return float("{0:0.1f}".format(gib))
+
+    @staticmethod
     def get_bytes():
         network = psutil.net_io_counters()
         return {'bytes_sent': network.bytes_sent, 'bytes_recv': network.bytes_recv}
 
+    def get_cpu(self):
+        f = open("/proc/stat", "r")
+        line = ""
+        while not "cpu " in line: line = f.readline()
+        f.close()
+        spl = line.split(" ")
+        worktime = int(spl[2]) + int(spl[3]) + int(spl[4])
+        idletime = int(spl[5])
+        dworktime = (worktime - self.last_worktime)
+        didletime = (idletime - self.last_idletime)
+        rate = float(dworktime) / (didletime + dworktime)
+        self.last_worktime = worktime
+        self.last_idletime = idletime
+        if (self.last_worktime == 0):
+            result = 0
+        else:
+            result = float("%.2f" % round(rate,2))*100
+        return result
+
     def run(self):
         """ Get sys info with 1s period """
         while True:
-            mem = str(os.popen('free -t -m').readlines())
-            """
-            Get a whole line of memory output, it will be something like below
-            ['             total       used       free     shared    buffers     cached\n', 
-            'Mem:           925        591        334         14         30        355\n', 
-            '-/+ buffers/cache:        205        719\n', 
-            'Swap:           99          0         99\n', 
-            'Total:        1025        591        434\n']
-             So, we need total memory, usage and free memory.
-             We should find the index of capital T which is unique at this string
-            """
-            T_ind = mem.index('T')
-            """
-            Than, we can recreate the string with this information. After T we have,
-            "Total:        " which has 14 characters, so we can start from index of T +14
-            and last 4 characters are also not necessary.
-            We can create a new sub-string using this information
-            """
-            mem_G = mem[T_ind + 14:-4]
-            """
-            The result will be like
-            1025        603        422
-            we need to find first index of the first space, and we can start our substring
-            from from 0 to this index number, this will give us the string of total memory
-            """
-            S1_ind = mem_G.index(' ')
-            mem_Total = mem_G[0:S1_ind]
-            """
-                    Similarly we will create a new sub-string, which will start at the second value. 
-                    The resulting string will be like
-                    603        422
-                    Again, we should find the index of first space and than the 
-                    take the Used Memory and Free memory.
-                    """
-            mem_G1 = mem_G[S1_ind + 8:]
-            S2_ind = mem_G1.index(' ')
-            mem_Used = mem_G1[0:S2_ind]
-            mem_Free = mem_G1[S2_ind + 8:]
-
-            self.cpu_used =  CPU_Pct=str(round(float(os.popen('''grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage }' ''').readline()),2))
-            memory = psutil.virtual_memory()
-            self.memory_total = mem_Total
-            self.memory_used = mem_Used
+            tot_m, used_m, free_m = map(int, os.popen('free -t -m').readlines()[-1].split()[1:])
+            self.cpu_used = self.get_cpu()
+            self.memory_total = self.mb_to_gib(tot_m)
+            self.memory_used = self.mb_to_gib(used_m)
             tx = self.get_bytes()['bytes_sent']
             rx = self.get_bytes()['bytes_recv']
 
